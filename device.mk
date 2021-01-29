2488b6bb9a1c36aae08b158c0d8570ea8a1f61ba
Ricky Cheung <rcheung844@gmail.com>
2021-01-29 10:42:59 +0800

    A37: Use camera device 3.2
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- device.mk
+++ device.mk
@@ -121,7 +121,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.device@1.0-impl \
+    camera.device@3.2-impl \
     libshim_camera \
     libcamera_shim \
     camera.msm8916 \
