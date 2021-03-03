cc685b4d61e2d69647ea3c13e8559e1a2e090fde
Ricky Cheung <rcheung844@gmail.com>
2021-03-03 12:21:15 +0800

    A37: Include overlay-lineage
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>
    Change-Id: Ia0c46ef1ab9eee75724f600b90348a2553523149

--- device.mk
+++ device.mk
@@ -172,7 +172,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
