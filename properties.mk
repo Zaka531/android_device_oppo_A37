f82d24c3652418b2941f541bb5ede08d0da5b9d1
Ricky Cheung <rcheung844@gmail.com>
2021-03-20 10:40:06 +0800

    A37: Update USB config
    Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
    Co-authored-by: anders3408 <anders3408@yahoo.dk>
    Co-authored-by: Danny Baumann <dannybaumann@web.de>
    Co-authored-by: Michael Bestas <mikeioannina@cyanogenmod.org>
    Co-authored-by: Jerry Zhang <zhangjerry@google.com>
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>
    Change-Id: I3a8a40a5e45b57781c8af01429daa73266dfa5fb

--- properties.mk
+++ properties.mk
@@ -130,18 +130,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=9,1 \
     persist.data.netmgrd.qos.enable=false
 
-# USB ID
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.usb.id.midi=90BA \
-    ro.usb.id.midi_adb=90BB \
-    ro.usb.id.mtp=2281 \
-    ro.usb.id.mtp_adb=2282 \
-    ro.usb.id.ptp=2284 \
-    ro.usb.id.ptp_adb=2283 \
-    ro.usb.id.ums=2286 \
-    ro.usb.id.ums_adb=2285 \
-    ro.usb.vid=2970
-
 # VoLTE
 PRODUCT_PROPERTY_OVERRIDES += \
     net.lte.volte_call_capable=true \
