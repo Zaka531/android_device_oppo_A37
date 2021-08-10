edd0313eb1db5ac678dd974abe98bb02ac9ce424
Ricky Cheung <rcheung844@gmail.com>
2021-08-10 14:36:16 +0800

    A37: Fix product information
    Change-Id: Iecc894f45f666f642225bc2481edad39c905e190

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+BOARD_VENDOR := oppo
+
 DEVICE_PATH := device/oppo/A37
 
 FORCE_32_BIT := true
